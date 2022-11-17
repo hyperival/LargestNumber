93a6faaecaa57081c98eed9518a34bd63173e63a
Hyperival <sanjaygautam779@gmail.com>
2022-11-17 17:06:23 +0530

    code of largest among 3 nummber is added

---
+++ LargestNumber/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LargestNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+
+            Console.WriteLine("Enter Three Number");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(" ");
+
+            if (a > b && b < c) {
+                Console.WriteLine("a is greater");
+            }
+            else if(b > c && b < a) {
+                Console.WriteLine("b is greater");
+
+            }
+            else
+            {
+                Console.WriteLine("c is greater");
+                    }
+        }
+    }
+}
